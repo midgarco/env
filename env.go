ee719753751e26d68801bf290a06311e2c607090
Jeff Dupont <jdupont@getcake.com>
2019-05-15 15:59:10 -0700

    Added methods to help with retrieving values

--- env.go
+++ env.go
@@ -9,7 +9,20 @@ import (
 
 // Load will load an ini formatted file into
 // the os ENVIRONMENT
-func Load(name string) error {
+func Load(path string, opts ...Option) error {
+	if !strings.HasSuffix(path, "/") {
+		path = path + "/"
+	}
+	err := load(path + ".env")
+	if err != nil {
+		return err
+	}
+	// Set override options
+	Override(opts...)
+	return nil
+}
+
+func load(name string) error {
 	f, err := os.Open(name)
 	if err != nil {
 		return err
@@ -40,4 +53,38 @@ func Load(name string) error {
 		}
 	}
 	return nil
+}
+
+// Option allows you to provide addtional settings to override
+type Option struct {
+	Key   string
+	Value string
+}
+
+// Get returns the value from the envronment config
+func Get(key string) string {
+	return os.Getenv(key)
+}
+
+// GetWithDefault returns the value from the environment config
+// or returns a default value if the setting is empty
+func GetWithDefault(key, def string) string {
+	s := os.Getenv(key)
+	if s == "" {
+		s = def
+	}
+	return s
+}
+
+// Override environment config with additional options
+func Override(opts ...Option) {
+	for _, opt := range opts {
+		os.Setenv(opt.Key, opt.Value)
+	}
+}
+
+// GetBool returns a boolean configuration setting
+func GetBool(key string) bool {
+	v := GetWithDefault(key, "false")
+	return (strings.ToLower(v) == "true" || v == "1")
 }
